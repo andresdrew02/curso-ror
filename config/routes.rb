821255bd6f2e7c004bcaedacce6d2556feab2d01
Andres del cerro <alumno.254958@ies-azarquiel.es>
2023-04-30 21:48:08 +0200

    routes refactored

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   # root "articles#index"
-  post '/products', to: 'products#create'
-  patch '/products/:id', to: 'products#update'
-  delete '/products/:id', to: 'products#destroy'
-  get '/products/new', to: 'products#new', as: :new_product
-  get '/products',to: 'products#index'
-  get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :edit_product
+  #post '/products', to: 'products#create'
+  #   patch '/products/:id', to: 'products#update'
+  #   delete '/products/:id', to: 'products#destroy'
+  #   get '/products/new', to: 'products#new', as: :new_product
+  #   get '/products',to: 'products#index'
+  #   get '/products/:id', to: 'products#show', as: :product
+  #   get '/products/:id/edit', to: 'products#edit', as: :edit_product
+
+  #el desglose de arriba son las mismas rutas creadas por la funcion resources
+  #path:'/' hace que las rutas de products, (/products) resida en el root
+  resources :products, path:'/'
 end
