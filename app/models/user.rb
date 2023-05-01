f72c8135fcf9ed58ded891909b7edd1dab89ca4b
Andres del cerro <alumno.254958@ies-azarquiel.es>
2023-05-01 18:31:06 +0200

    user_product relation and migration, and create product with current user stablish in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
             }
   validates :password_digest, length: { minimum: 6 }
 
+  has_many :products, dependent: :destroy
   before_save :downcase_attributes
 
   private
